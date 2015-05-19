0d960d08302a8e4fca6c320f71f20dc0f439fb27
Jesper Kihlberg <jesper.kihlberg@gmail.com>
2015-05-19 21:15:06 +0200

    added department and designer wordpress shortcode

---
+++ department.php
@@ -0,0 +1,19 @@
+<?php
+function department_func( $atts ) {
+    $a = shortcode_atts( array(
+        'shopid' => '',
+        'locale' => '',
+        'departmentid' => '1',
+        'categorycount' => '10',
+        'departmenturl' => '/index.php/maend',
+        'basecategoryurl' => '/index.php/kategori',
+    ), $atts );
+  
+  $shopId = $a['shopid'];// 'http://monshirtdk.spreadshirt.dk';
+  $locale = '?locale='.$a['locale'];//'/dk/DK';
+  $departmentId = $a['departmentid'];
+  $categorycount = $a['categorycount'];
+  $departmenturl = $a['departmenturl'];
+  $basecategoryurl = $a['basecategoryurl'];
+  DrawDepartmentId($locale,$shopId, $departmentId,$categorycount,$departmenturl, $basecategoryurl);
+}
---
+++ spreadshirt-designer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @package Spreadshirt-Designer
+ */
+/*
+Plugin Name: Spreadshirt Designer
+Plugin URI: http://jesperkihlberg.dk/spreadshirtdesigner
+Description: Allows you to add a spreadshirt designer on your wordpress site. The designer allows for url parameters to be passed. The following parameters are supported: productid, product, designid, productcolor, designcolor1, designcolor2, department. The correspondent ids are found by using the spreadshirt api. Work is in progress to add this information to the plugin as well.
+Version: 1.0.0
+Author: Jesper Kihlberg
+Author URI: http://blog.jesperkihlberg.dk/
+License: GNU General Public License v2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+Text Domain: spreadshirt-designer
+*/
+
+/*
+This theme, like WordPress, is licensed under the GPL.
+Use it to make something cool, have fun, and share what you've learned with others.
+*/
+include("functions.php");
+include("designer.php");
+include("products.php");
+include("department.php");
+
+add_shortcode( 'designer', 'designer_func' );
+add_shortcode( 'products', 'products_func' );
+add_shortcode( 'department', 'department_func' );
